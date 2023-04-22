828edd3234cdc7b3a55eb5bc65c86760bc535ec7
HARSH RAJ <119311590+Garudaboss122@users.noreply.github.com>
2023-04-22 15:25:55 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -488,10 +488,10 @@ async def about_handler(c: Client, m: Message):
         quote=True,
         reply_markup=InlineKeyboardMarkup(
             [
-                [InlineKeyboardButton("👨‍💻Developer👨‍💻", url="https://t.me/yashoswalyo")],
+                [InlineKeyboardButton("👨‍💻Developer👨‍💻", url="https://t.me/PROFE07XH")],
                 [
                     InlineKeyboardButton(
-                        "🏘Source Code🏘", url="https://github.com/yashoswalyo/MERGE-BOT"
+                        "🏘Code By🏘", url="https://t.me/PROFE07XH"
                     ),
                     InlineKeyboardButton(
                         "🤔Deployed By🤔", url=f"https://t.me/{Config.OWNER_USERNAME}"
